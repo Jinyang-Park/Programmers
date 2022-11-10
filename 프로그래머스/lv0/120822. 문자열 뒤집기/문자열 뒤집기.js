5f3250e53933da01af84e887356750b9f81d44e9
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-11-10 19:22:47 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = '';
+    let arr = String(my_string).split("");
+    
+    for(let i = arr.length-1; i >= 0; i--){
+        answer += arr[i]
+    }
+    return answer;
+}
