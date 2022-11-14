4bc6bf08274aacae617f563653b2eb316089c18f
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-11-14 20:03:58 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,4 +1,8 @@
-
 function solution(dot) {
-    return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] > 0 ? 2 : 3;
+    var answer = 0;
+    if(dot[0] > 0 && dot[1] > 0) answer = 1;
+    if(dot[0] < 0 && dot[1] > 0) answer = 2;
+    if(dot[0] < 0 && dot[1] < 0) answer = 3;
+    if(dot[0] > 0 && dot[1] < 0) answer = 4;
+    return answer;
 }
