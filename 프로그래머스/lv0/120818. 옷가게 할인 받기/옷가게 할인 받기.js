4c60377ba1e4daa932ed52cb8827c19495fd1f25
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-02 12:18:18 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,34 +1,26 @@
-
 // function solution(price) {
 //     // 화살표 함수로 사용하면 안된다! 
 //     //  >= 0 이렇게 써야된다!
     
 //     var answer = 0;
 //     if(price >= 500000){
-//          answer = price - (price * 0.2);
-//     }else if(price >= 300000 && price < 500000){
+//          answer =  price - (price * 0.2);
+//     }else if(price >= 300000 ){
 //           answer = price - (price * 0.1);
-//     }else if(price >= 100000 && price < 300000){
+//     }else if(price >= 100000 ){
 //        answer = price - (price * 0.05);
 //     }else{
-//         answer;
+//         answer = price;
 //     }
 //     return Math.floor(answer);
 // }
 
 function solution(price) {
-    // 화살표 함수로 사용하면 안된다! 
-    //  >= 0 이렇게 써야된다!
-    
-    var answer = 0;
-    if(price >= 500000){
-         answer =  Math.floor(price - (price * 0.2));
-    }else if(price >= 300000 && price < 500000){
-          answer = Math.floor(price - (price * 0.1));
-    }else if(price >= 100000 && price < 300000){
-       answer = Math.floor(price - (price * 0.05));
-    }else{
-        answer = price;
-    }
-    return answer;
+
+    let arr_discount = [[500000, 0.2], [300000, 0.1], [100000, 0.05]]
+
+    for(let i = 0 ; i < arr_discount.length ; i++)
+        if(arr_discount[i][0] <= price) return Math.floor(price * (1-arr_discount[i][1]))        
+
+    return price
 }
