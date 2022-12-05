3c3750360c18b4b2f95081a41134bbff2e607210
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-05 23:31:08 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,3 +1,11 @@
-function solution(my_string) {    
-    return my_string.replace(/[aeiou]/g,'') 
+function solution(my_string) {
+    let answer = '';
+    let alpa=['a','e','i','o','u']
+    
+    for(let i=0; i< my_string.length; i++){
+        if(!alpa.includes(my_string[i])){
+            answer+=my_string[i]
+        }
+    }
+    return answer;
 }
