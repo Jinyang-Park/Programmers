e2dda7b0d95a79482bbd280d4ac74048293a0880
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-06 10:09:32 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,10 +1,20 @@
 function solution(num_list, n) {
     var answer = [];
-    
-    for(let i=0; i < num_list.length / n; i++){
-        answer = [...answer, num_list.slice(i*n, i * n + n)];
-        console.log
+
+    // i = 0 일때, 
+    // 한바퀴 돌고 2 = 0+2
+    // i = 2 일때, 
+    // 한바퀴 돌고 4 = 2+2
+    // i = 4 일때,
+    // 한바퀴 돌고 6 = 4+2
+    // i = 6 일때,
+    for (let i = 0; i<num_list.length; i=i+n){
+        // slice(0,2) 푸시이면 [[1,2]]
+        // slice(2,4) 푸시이면 [[1,2],[3,4]]
+        // slice(4,6) 푸시이면 [[1,2],[3,4],[5,6]]
+        // slice(6,8) 푸시이면 [[1,2],[3,4],[5,6],[7,8]]
+        answer.push(num_list.slice(i, i+n));
     }
-    
+
     return answer;
 }
