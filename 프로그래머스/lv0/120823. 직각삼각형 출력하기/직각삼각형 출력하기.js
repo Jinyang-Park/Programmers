99a4452fddd0435f4de1c4d6fb6977820e3afafb
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-07 11:01:50 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 53.19 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -9,7 +9,11 @@ let input = [];
 rl.on('line', function (line) {
     input = line.split(' ');
 }).on('close', function () {
+    //close가 입력값 3이온다
+    //for문을 돌면서 close는 한줄에 대한 별의 갯수를 출력
     close = (Number(input[0]));
+    // i=1이여야 첫번째 줄 * 출력
+    // i += 1 --> i = i+1 --> 2 = 1+1 --> 3 = 2+1
     for(let i =1; i <= close; i+=1){
         console.log('*'.repeat(i))
     }
