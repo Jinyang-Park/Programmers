51a2696458939e3c49ae22092c78e77169c43dca
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-07 10:45:06 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.js
@@ -1,5 +1,8 @@
 function solution(n, t) {
+    // answer를 n으로 준 이유는 2만큼씩 커져야된다.
     var answer = n;
+    // let i = 1을 줘야 t값만큼 for문이 돌면서 answer*2를 하기 때문이다.
+    // let i = 0으로 시작하면 한번더 answer가 곱해져서 4096값이 나온다.
     for(let i=1; i <= t; i++){
         answer = answer*2
         
