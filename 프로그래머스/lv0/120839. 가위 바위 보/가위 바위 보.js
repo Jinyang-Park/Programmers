ed1a13b92d147fdabb1ad24abbed9d871c6ea4cd
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-08 12:57:05 +0900

    [level 0] Title: 가위 바위 보, Time: 3.66 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,18 @@
+function solution(rsp) {
+    // 가위 2
+    // 바위 0
+    // 보 5
+    var answer = '';
+    let arr = rsp.split('');
+    console.log(arr)
+    for(let i =0; i < arr.length; i++){
+     if(arr[i] == 2){
+        answer += '0';
+     }else if(arr[i] == 0){
+        answer += '5';
+     }else if (arr[i] == 5){
+        answer += '2';   
+     }
+    }
+    return answer;   
+}
