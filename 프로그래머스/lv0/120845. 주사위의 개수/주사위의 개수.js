f73a80e31480044b0eeb75162b571fbf82fba874
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-08 14:26:09 +0900

    [level 0] Title: 주사위의 개수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.js
@@ -1,4 +1,6 @@
 function solution(box, n) {
-    return box.map((x) => Math.floor(x/n)).reduce((a,b) => (a) * (b));
-    
+    return box.map((x) => Math.floor(x/n)).reduce((a,b) => a * b);
+    //reduce((3, 2) => 3 * 2)
+    //reduce((6, 2) => 6 * 2)
+    // 12
 }
