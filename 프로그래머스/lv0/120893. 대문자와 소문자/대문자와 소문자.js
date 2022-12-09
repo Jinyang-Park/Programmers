2b1d2bbaa90c2a65b94561240a62f882594b813f
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-09 10:18:41 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,17 +1,17 @@
-// function solution(my_string){
-//     return my_string.split('').map((s) => s === s.toLowerCase()? s.toUpperCase() : s.toLowercase()).join('');
-// }
-
-
-function solution(my_string) {
-    var answer = '';
-    
-    for(let x of my_string){
-        if(x === x.toLowerCase()){
-            answer += x.toUpperCase()
-        }else{
-            answer += x.toLowerCase()
-        }
-    }
-    return answer;
+function solution(my_string){
+    return my_string.split('').map((s) => (s === s.toLowerCase()? s.toUpperCase() : s.toLowerCase())).join('');
 }
+
+
+// function solution(my_string) {
+//     var answer = '';
+    
+//     for(let x of my_string){
+//         if(x === x.toLowerCase()){
+//             answer += x.toUpperCase()
+//         }else{
+//             answer += x.toLowerCase()
+//         }
+//     }
+//     return answer;
+// }
