966ef5b972ae0fbf501a3d81c351d008abc5bb6b
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-09 10:13:44 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.29 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    var answer = '';
+    
+    for(let x of my_string){
+        if(x === x.toLowerCase()){
+            answer += x.toUpperCase()
+        }else{
+            answer += x.toLowerCase()
+        }
+    }
+    return answer;
+}
