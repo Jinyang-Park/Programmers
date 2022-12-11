337c44e2564ae0aed2e5e4c5f28bb32544a872c3
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-11 22:39:12 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,10 +1,15 @@
 function solution(age) {
     let alpha =["a","b","c","d","e","f","g","h","i","j"];
-    var answer = '';
-    age = age.toString();
+ // let age =  [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9 ];
+    var answer = ''
+    age = age.toString().split("");
+    //['2','3']
     
     for(let i =0; i< age.length; i++){
         answer += alpha[age[i]]
+        // console.log(age[i]) --> 2 --> alpha[2] --> c
+        // console.log(age[i]) --> 3 --> alpha[3] --> d
     }
     return answer;
+    // 'cd'
 }
