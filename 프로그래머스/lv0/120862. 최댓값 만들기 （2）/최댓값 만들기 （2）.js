03b83fdbdc76f2efc3f4d24961b52c35e9eb724c
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2022-12-13 10:00:15 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -2,10 +2,22 @@ function solution(numbers) {
     var answer = [];
     for(let i =0; i < numbers.length; i++){
         for(let j=0; j < numbers.length; j++){
+            // i =0 이면 j=0 x 
+            // i =0 j=1 곱하면 2
+            // i =0 j=2 곱하면 3
+            // i =0 j=3 곱하면 4
+            // i =0 j=4 곱하면 5
+            // i =1 이면 j=0 곱하면 2
+            // i =1 j=1 x
+            // i =1 j=2 곱하면 6
+            // i =1 j=3 곱하면 8
+            // i =1 j=4 곱하면 5 
             if(i!==j){
                 answer.push(numbers[i]*numbers[j])
             }
         }
     }
      return Math.max(...answer);
+    // ...answer에는 ['2','3','4','5','6','8',...]
+    // Math.max하면 큰 값을 리턴
 }
