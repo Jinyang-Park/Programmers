aa97b95ad6ae8329a5802cb76b85cbe0c6766c43
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2023-04-06 21:12:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 3.67 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,6 +1,11 @@
 function solution(my_string) {
-    var answer = 0;
-    let aa = my_string.split/[0-9]+/g
-    console.log(aa)
-    return answer;
+    // /\d+/g 숫자만 골라준다.
+    let a = my_string.match(/[0-9]+/g);
+    console.log(a)
+   // [ '1', '2', '34' ] 
+    return a ? a.reduce((b,c) => b+ Number(c),0) : 0
+    // 문자열이므로 Number(c) 로하면 1 , 2 , 34 가 됨
+    // 0 + 1 = 1
+    // 1 + 2 = 3
+    // 34 + 3 = 37
 }
