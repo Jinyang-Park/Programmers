8aefa99aeb6c84a287f742897d7d10a73fc501f1
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2023-04-06 17:15:24 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,4 +1,10 @@
 function solution(emergency) {
     let max = [...emergency].sort((a, b)=> b-a)
+    // 큰 수 대로 정렬됨
+    // console.log(max)
+    // [76, 24, 3]
     return emergency.map(x => max.indexOf(x)+1)
+    // emergency.map을 돌리면 x는 3 -> max.indexOf(3) -> 2+1 -> [3]
+    // emergency.map을 돌리면 x는 76 -> max.indexOf(76) -> 0+1 -> [3,1]
+    // emergency.map을 돌리면 x는 24 -> max.indexOf(24) -> 1+1 -> [3,1,2]
 }
