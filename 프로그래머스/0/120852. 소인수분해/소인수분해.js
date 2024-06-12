cf7b732280bb79786860aae0f2a9558753640d92
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2024-06-12 14:28:09 +0900

    [level 0] Title: 소인수분해, Time: 5.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,13 +1,14 @@
 function solution(n) {
     var result = [];
-    let divisor = 2;
+    let divisor = 2; // 3
     
-    while (n >= 2){ //12 >=2
-         if(n % divisor === 0) {// 12 % 2 = 6 
-             result.push(divisor)  // [2]
-n = n / divisor; // 6 = 12 / 2         
+    while (n >= 2){ //12 >=2 , 6>=2 , 3>=2 , 3>=2, 0 >= 3 (fasle)
+         if(n % divisor === 0) {// 12 % 2 = 6 , 6 % 2 = 3 , 3 % 2 -> 거짓 , 3%3 ===0
+         result.push(divisor) // [2] [2,2,3]
+        console.log(result)
+         n = n / divisor; // 6 = 12 / 2 , 3 = 6 / 2  , 0 = 3 / 3 
     }
-    else divisor++;
+    else divisor++; // 3이 if문에서 걸리 떄문에 3이 된다
 }
     return [...new Set(result)]
 }
