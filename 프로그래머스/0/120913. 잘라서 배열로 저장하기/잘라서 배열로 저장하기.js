14da65a93fc5e5ba9fe798b4a50b91e1090dc3e1
Jinyang Park <80263801+Jinyang-Park@users.noreply.github.com>
2024-06-12 11:43:22 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,11 @@
+function solution(my_str, n) {
+    // 1. slice로 n만큼 잘른다
+    // 2. 만약 my_str 10이면 3길이만큼 자른다.
+    // 3. 3, 3, 3, 1 이 남는다.
+    // 4. 1이 n보다 작으니깐 넣어준다.
+let num = [];
+    for(let i = 0; i < my_str.length / n; i++) {
+        num = [...num, my_str.slice(i*n,n+(i*n))]
+    }
+    return num;
+}
